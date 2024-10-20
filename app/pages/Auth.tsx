ce65d8fb65f29fc2a743b1a323a41355f8dbf39e
kishore28kumar <kishore28kks@gmail.com>
2024-10-20 20:29:38 +0530

    chore: add page

---
+++ app/pages/Auth.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Auth = () => {
+  return (
+    <div>
+      <div className='text-xl font-'>Mail Classifier</div>
+    </div>
+  )
+}
+
+export default Auth
---
+++ app/pages/Mail.tsx
@@ -0,0 +1,11 @@
+import { Container } from '../componets/Container'
+import { NavBar } from '../componets/NavBar'
+
+export const Mail = () => {
+  return (
+    <div>
+        <NavBar />
+        <Container />
+    </div>
+  )
+}
